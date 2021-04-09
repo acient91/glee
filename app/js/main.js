61115d99544b4b95efa7991fd20ba2a377ed8812
acient91 <speqtr.91@gmail.com>
2021-04-10 00:36:27 +0300

    добавил 3 секции

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,18 @@ $(function(){
     autoplaySpead: 2000
   });
 
+  $('.partners__item').slick({
+    dots: false,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpead: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+  });
+
 
 
   var mixer = mixitup('.products__items');
+  var mixer = mixitup('.design__items');
 });
