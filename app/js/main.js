8b3ed1fd3d9bc14e91df035f6acf624b525a2e82
acient91 <speqtr.91@gmail.com>
2021-05-08 15:12:06 +0300

    added block price, and block category

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,19 @@ $(function(){
     slidesToScroll: 1,
   });
 
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
 
   const mixin1 = document.querySelector('.products__items');
   const mixin2 = document.querySelector('.design__items');
