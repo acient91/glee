6946e7d5e7ebcff6ffbee608bd3c983a928b7030
acient91 <speqtr.91@gmail.com>
2021-05-30 20:05:42 +0300

    Сделал адаптив heder

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function () {
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+  })
+
   $('.product-one__tabs-link').on('click', function(e){
     e.preventDefault();
     $('.product-one__tabs-link').removeClass('product-one__tabs-link--active');
